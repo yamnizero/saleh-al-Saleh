f526189798b687008521484043ff3fb1aa1a9290
mohammed jamal <yamni.zero@gmail.com>
2024-03-11 13:23:43 +0200

    -Language -add translations - create function

--- lang.js
+++ lang.js
@@ -1,11 +1,42 @@
-let lang ={
-    //AR
-    ar:{
+const translations = {
+    en: {
+        //header
+        home: 'Home',
+        service: 'Service',
+        about: 'About US',
+        contact: 'Contact Us',
+        // body
+        Saleh_Al_Saleh: "Saleh Al Saleh Law Firm and Legal Consulting",
+
+    },
+
+    ar: {
+        //header
+        home: "الصفحة الرئيسية",
+        service: 'خدماتنا',
+        about: 'من نحن',
+        contact: 'تواصل معنا',
+        // body
         Saleh_Al_Saleh: "صالح ال صالح للمحاماة و الاستشارات القانونية",
     },
-    //EN
-    en:{
-        Saleh_Al_Saleh: "Saleh Al Saleh Law Firm and Legal Consulting",
-    }
-}
+};
 
+
+const languageSelector = document.querySelector('select');
+languageSelector.addEventListener('change', (event) => {
+    setLanguage(event.target.value);
+    localStorage.setItem('lang',event.target.value)
+
+});
+document.addEventListener('DOMContentLoaded', () => {
+   
+    setLanguage(localStorage.getItem("lang"));
+})
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll("[data-i18n]");
+    elements.forEach((element) =>{
+        const translationsKey = element.getAttribute("data-i18n");
+        element.textContent = translations[language][translationsKey]
+    });
+    document.dir = language == "ar" ? "rtl" :"ltr";
+};
