8fe3af53e509937aca43d2a471f20d61f4be60d4
mohammed jamal <yamni.zero@gmail.com>
2024-03-11 12:18:40 +0200

    lang

---
+++ lang.js
@@ -0,0 +1,11 @@
+let lang ={
+    //AR
+    ar:{
+        Saleh_Al_Saleh: "صالح ال صالح للمحاماة و الاستشارات القانونية",
+    },
+    //EN
+    en:{
+        Saleh_Al_Saleh: "Saleh Al Saleh Law Firm and Legal Consulting",
+    }
+}
+
