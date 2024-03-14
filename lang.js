ed120ee0d4713f2a51ef4359e8435607239c69bb
mohammed jamal <yamni.zero@gmail.com>
2024-03-14 14:26:11 +0200

    header

--- lang.js
+++ lang.js
@@ -7,6 +7,11 @@ const translations = {
         contact: 'Contact Us',
         // body
         Saleh_Al_Saleh: "Saleh Al Saleh Law Firm and Legal Consulting",
+        body_index:"Saleh Al Saleh Law Firm and Legal Consulting is one of the most prominent legal services providers in the UAE through legal expertise and in professional ways, ensuring transactional satisfaction from any legal problems facing our valued clients. The Office deals in all kinds of civil, commercial and criminal cases and provides legal services in various fields and at all levels.",
+        
+        
+        
+        
         english: 'English',
         arabic: 'عربي',
 
@@ -20,6 +25,11 @@ const translations = {
         contact: 'تواصل معنا',
         // body
         Saleh_Al_Saleh: "صالح ال صالح للمحاماة و الاستشارات القانونية",
+        body_index:"مكتب صالح الصالح للمحاماة والاستشارات القانونية هو واحد من أبرز مقدمي الخدمات القانونية في الإمارات العربية المتحدة من خلال الخبرة القانونية والطرق المهنية، مضمونًا رضى العملاء من المشاكل القانونية التي تواجهها. يتعامل المكتب في جميع أنواع القضايا المدنية والتجارية والجنائية ويقدم خدمات قانونية في مختلف المجالات وعلى جميع المستويات.",
+
+
+
+
         english: 'English',
         arabic: 'عربي',
     },
