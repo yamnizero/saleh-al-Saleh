85827c7bba093af071a5395bb344701237736540
mohammed jamal <yamni.zero@gmail.com>
2024-03-29 15:31:06 +0200

    create php

---
+++ create.php
@@ -0,0 +1,425 @@
+<?php
+    include "logic.php"
+?>
+<html lang="en">
+
+<head>
+
+    <title>Saleh Al Saleh</title>
+    <meta name="description" content="تقديم  أفضل خدمات قانونية بكل احترافية و شفافية ونزاهة للدفاع عن حقوق ومصالح الموكلين والحفاظ على مكتسباتهم ">
+    <meta charset="utf-8">
+    <meta name="author" content="Phoenix Code">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Favicon -->
+    <link rel="icon" href="assests/images/Logo.gif" type="image/png" sizes="any">
+        <link rel="icon" href="assests/images/Logo.gif" type="image/svg+xml">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assests/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assests/css/style.css">
+    
+    <link rel="stylesheet" href="assests/css/style-responsive.css">
+    <link rel="stylesheet" href="assests/css/vertical-rhythm.min.css">
+    <link rel="stylesheet" href="assests/css/magnific-popup.css">
+    <link rel="stylesheet" href="assests/css/owl.carousel.css">
+    <link rel="stylesheet" href="assests/css/splitting.css">
+    <link rel="stylesheet" href="assests/css/YTPlayer.css">
+    <link rel="stylesheet" href="assests/css/demo-main/demo-main.css">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
+</head>
+
+<body class="appear-animate dark-mode">
+
+    <!-- Page Loader -->
+    <div class="page-loader dark">
+        <div class="loader">Loading...</div>
+    </div>
+    <!-- End Page Loader -->
+
+    <!-- Skip to Content -->
+    <a href="#main" class="btn skip-to-content">Skip to Content</a>
+    <!-- End Skip to Content -->
+
+    <!-- Page Wrap -->
+    <div class="page bg-dark-1" id="top">
+
+        <!-- Navigation Panel -->
+        <nav class="main-nav dark dark-mode transparent stick-fixed wow-menubar">
+            <div class="main-nav-sub full-wrapper">
+
+                <!-- Logo  (* Add your text or image to the link tag. Use SVG or PNG image format. 
+                    If you use a PNG logo image, the image resolution must be equal 200% of the visible logo
+                    image size for support of retina screens. See details in the template documentation. *) -->
+                <div class="p-2">
+
+                    <a href="index.html" class="">
+                        <img src="assests/images/logo3.gif" alt="" width="150" height="50" />
+                        <!-- <img src="assests/images/text.gif" alt="Your Company Logo" width="105" height="34" /> -->
+                    </a>
+
+                </div>
+
+                <!-- Mobile Menu Button -->
+                <div class="mobile-nav" role="button" tabindex="0">
+                    <i class="mobile-nav-icon"></i>
+                    <span class="visually-hidden">Menu</span>
+                </div>
+
+                <!-- Main Menu -->
+                <div class="inner-nav desktop-nav">
+                    <ul class="clearlist local-scroll">
+
+                        <!-- Item With Sub -->
+                        <li>
+                            <a href="index.html" data-i18n="home" class="">Home <i class=""></i></a>
+
+
+
+                        </li>
+                        <!-- End Item With Sub -->
+
+                        <!-- Item With Sub -->
+                        <li>
+                            <a href="#" data-i18n="service" class="mn-has-sub">Service <i
+                                    class="mi-chevron-down"></i></a>
+
+                            <!-- Sub Megamenu -->
+                            <ul class="mn-sub mn-has-multi">
+
+                                <li class="mn-sub-multi">
+                                    <!-- <span class="mn-group-title">Group 1</span> -->
+
+                                    <ul>
+                                        <li>
+                                            <a href="legal-consulyation.html" data-i18n="LegalConsultations">Legal
+                                                Consultations</a>
+                                        </li>
+                                        <li>
+                                            <a href="preparation-and-drafting-of-legal-documents.html"
+                                                data-i18n="legalDocuments">Preparation and drafting of legal
+                                                documents</a>
+                                        </li>
+                                        <li>
+                                            <a href="legal-procedures-registration-and-legal-credits.html"
+                                                data-i18n="LegalProcedures">Legal procedures, registration and
+                                                legal credits</a>
+                                        </li>
+                                        <li>
+                                            <a href="legal-representation.html" data-i18n="LegalRepresentation">Legal
+                                                Representation</a>
+                                        </li>
+
+                                    </ul>
+
+                                </li>
+
+                                <!-- <li class="mn-sub-multi"> -->
+
+
+                                    <!-- <ul>
+                                        <li>
+                                            <a href="research-services.html" data-i18n="ResearchServices">Research
+                                                services</a>
+                                        </li>
+                                        <li>
+                                            <a href="penal-cases.html" data-i18n="PenalCases">Penal cases</a>
+                                        </li>
+                                        <li>
+                                            <a href="criminal-cases.html" data-i18n="CriminalCases">Criminal Cases</a>
+                                        </li>
+                                        <li>
+                                            <a href="cybercrime-lawsuit.html" data-i18n="CybercrimeLawsuit">Cybercrime
+                                                Lawsuit</a>
+                                        </li>
+                                        <li>
+                                            <a href="labor-lawsuit.html" data-i18n="LaborLawsuit">Labor Lawsuit</a>
+                                        </li>
+                                        <li>
+                                            <a href="rent-disputes.html" data-i18n="RentDisputes">Rent Disputes</a>
+                                        </li>
+                                        <li>
+                                            <a href="family-issues-and-conditions.html"
+                                                data-i18n="FamilyIssuesAndConditions">Family issues and conditions</a>
+                                        </li>
+                                        <li>
+                                            <a href="insurance.html" data-i18n="Insurance">Insurance</a>
+                                        </li>
+                                    </ul> -->
+
+                                <!-- </li> -->
+
+                                <!-- <li class="mn-sub-multi"> -->
+                                    <!-- <span class="mn-group-title">Group 3</span> -->
+
+                                    <!-- <ul>
+                                        <li>
+                                            <a href="maritime-disputes.html" data-i18n="MaritimeDisputes">Maritime
+                                                disputes</a>
+                                        </li>
+                                        <li>
+                                            <a href="banking-and-finance.html" data-i18n="BankingAndFinance">Banking and
+                                                Finance</a>
+                                        </li>
+                                        <li>
+                                            <a href="trading-companies.html" data-i18n="TradingCompanies">Trading
+                                                companies</a>
+                                        </li>
+                                        <li>
+                                            <a href="litigation-and-dispute-resolution.html"
+                                                data-i18n="LitigationAndDisputeResolution">Litigation and Dispute
+                                                Resolution</a>
+                                        </li>
+                                        <li>
+                                            <a href="precautionary-measures.html"
+                                                data-i18n="PrecautionaryMeasures">Precautionary measures</a>
+                                        </li>
+                                        <li>
+                                            <a href="executive-actions.html" data-i18n="ExecutiveActions">Executive
+                                                Actions</a>
+                                        </li>
+                                        <li>
+                                            <a href="real-estate-disputes.html" data-i18n="RealEstateDisputes">Real
+                                                estate disputes</a>
+                                        </li>
+                                        <li>
+                                            <a href="debt-collection.html" data-i18n="DebtCollection">Debt
+                                                Collection</a>
+                                        </li>
+                                    </ul> -->
+
+                                <!-- </li> -->
+
+                            </ul>
+                            <!-- End Sub Megamenu -->
+
+                        </li>
+                        <!-- End Item With Sub -->
+
+                        <!-- Item With Sub -->
+                        <li>
+                            <a href="about-us.html" data-i18n="about" class="">About Us <i class=""></i></a>
+
+
+
+                        </li>
+                        <!-- End Item With Sub -->
+
+                        <!-- Item With Sub -->
+                        <li>
+                            <a href="contact-us.html" data-i18n="contact" class="">Contact Us</a>
+                        </li>
+                        <!-- End Item With Sub -->
+                        <li>
+                            <a href="#" data-i18n="team" class="mn-has-sub active">Team <i class=""></i></a>
+
+
+
+                        </li>
+                        <!-- Item With Sub -->
+                        <li>
+                            <a href="#" class="mn-has-sub">Blog <i class="mi-chevron-down"></i></a>
+
+                            <!-- Sub -->
+                            <ul class="mn-sub">
+
+                                <li>
+                                    <a href="#" class="mn-has-sub">Classic <i class="mi-chevron-right right"></i></a>
+
+                                    <ul class="mn-sub">
+                                        <li>
+                                            <a href="main-blog-classic-sidebar-left-dark.html">Sidebar Left</a>
+                                        </li>
+                                        <li>
+                                            <a href="main-blog-classic-sidebar-right-dark.html">Sidebar Right</a>
+                                        </li>
+                                        <li>
+                                            <a href="main-blog-classic-fullwidth-dark.html">Fullwidth</a>
+                                        </li>
+                                    </ul>
+
+                                </li>
+
+                                <li>
+                                    <a href="#" class="mn-has-sub">Columns <i class="mi-chevron-right right"></i></a>
+
+                                    <ul class="mn-sub">
+                                        <li>
+                                            <a href="main-blog-columns-2col-dark.html">2 Columns</a>
+                                        </li>
+                                        <li>
+                                            <a href="main-blog-columns-3col-dark.html">3 Columns</a>
+                                        </li>
+                                    </ul>
+
+                                </li>
+
+                                <li>
+                                    <a href="#" class="mn-has-sub">Single <i class="mi-chevron-right right"></i></a>
+
+                                    <ul class="mn-sub">
+                                        <li>
+                                            <a href="main-blog-single-sidebar-left-dark.html">Sidebar Left</a>
+                                        </li>
+                                        <li>
+                                            <a href="main-blog-single-sidebar-right-dark.html">Sidebar Right</a>
+                                        </li>
+                                        <li>
+                                            <a href="main-blog-single-fullwidth-dark.html">Fullwidth</a>
+                                        </li>
+                                    </ul>
+
+                                </li>
+
+                            </ul>
+                            <!-- End Sub -->
+
+                        </li>
+                        <!-- End Item With Sub -->
+
+                    </ul>
+
+                    <ul class="items-end clearlist">
+                        <li>
+
+
+
+                            <select>
+                                <option value="en" selected data-i18n="english">English</option>
+                                <option value="ar" data-i18n="arabic">Arabic</option>
+                            </select>
+
+
+
+
+                        </li>
+                        <!-- End Languages -->
+
+                        <li><a href="https://wa.link/uaajb8" target="_blank" class="opacity-1 no-hover"><span 
+                            data-i18n="FreeLegalAdvice"
+                                    class="link-hover-anim underline" data-link-animate="y">
+                                    Free Legal
+                                    Advice
+                                </span></a></li>
+
+                    </ul>
+
+                </div>
+                <!-- End Main Menu -->
+                
+            </div>
+        </nav>
+        <!-- End Navigation Panel -->
+
+        <main id="main">
+
+           
+
+
+            <!-- About Section -->
+            <section class="page-section bg-dark-1 light-content pb-0" id="about">
+                
+
+                        <div class="container mt-5" >
+                           <form action="POST">
+                            
+                            <input type="file" name="fileToUpload" id="fileToUpload">
+                            <input type="text" placeholder="Blog Title" class="form-control bg-dark text-white my-3 text-center">
+                            <textarea class="form-control bg-dark text-white my-3 "></textarea>
+                            <button class="btn btn-dark">Add Post</button>
+                           </form>
+                        </div>
+
+               
+                  
+            </section>
+            
+            
+
+
+            <!-- Experience Section -->
+            
+            <!-- End Experience Section -->
+
+
+            <!-- Divider -->
+            <hr class="white opacity-015 mt-0 mb-0" />
+            <!-- End Divider -->
+
+
+        
+
+
+           
+
+
+        </main>
+
+        <!-- Footer -->
+        <footer class="bg-dark-2 light-content footer z-index-1 position-relative">
+            <div class="container position-relative text-center pt-140 pb-80 pb-sm-50">
+                
+               
+                
+                <!-- Social Links -->
+                <div class="footer-social-links mb-60">
+                    <a href="https://www.facebook.com/profile.php?id=100078678734849" title="Facebook" rel="noopener nofollow" target="_blank">
+                        <span class="visually-hidden">Facebook</span>
+                        <i class="fa-facebook-f"></i>
+                    </a>
+                    <a href="https://twitter.com/saleh_firm" title="Twitter" rel="noopener nofollow" target="_blank">
+                        <span class="visually-hidden">Twitter</span>
+                        <i class="fa-twitter"></i>
+                    </a>
+                    <a href="https://maps.app.goo.gl/dQ3AR3SZioq1kndQ9" title="Google" rel="noopener nofollow" target="_blank">
+                        <span class="visually-hidden">Google</span>
+                        <i class="fa-google"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/saleh-al-saleh-advocates-and-legal-consutants-4682a0225" title="LinkedIn" rel="noopener nofollow" target="_blank">
+                        <span class="visually-hidden">LinkedIn</span>
+                        <i class="fa-linkedin"></i>
+                     </a>
+                    
+                </div>
+                <!-- End Social Links -->
+                
+                <!-- Footer Text -->
+                <div class="footer-text">
+                    
+                    <!-- Copyright -->
+                    <div>
+                        <a href="#">© Saleh-Al-Saleh 2024</a>.
+                    </div>
+                    <!-- End Copyright -->
+                    
+                   
+                    
+                </div>
+                <!-- End Footer Text -->  
+                
+             </div>
+             
+        </footer>
+        <!-- End Footer -->
+
+    </div>
+    <!-- End Page Wrap -->
+
+    <!-- JS -->
+    <script src="assests/js/jquery.min.js"></script>
+    <script src="assests/js/bootstrap.bundle.min.js"></script>
+    <script src="assests/js/plugins.js"></script>
+    <script src="assests/js/jquery.ajaxchimp.min.js"></script>
+    <script src="assests/js/contact-form.js"></script>
+    <script src="assests/js/all.js"></script>
+    <script src="lang.js" type="module"></script>
+    <!-- End JS -->
+
+</body>
+
+</html>
