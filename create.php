ffbe0192208a1d0fdc46b5a3d27447faa1855dca
mohammed jamal <yamni.zero@gmail.com>
2024-03-30 04:56:17 +0200

    - create function php -done to upload to data

--- create.php
+++ create.php
@@ -322,16 +322,16 @@
 
 
             <!-- About Section -->
-            <section class="page-section bg-dark-1 light-content pb-0" id="about">
+            <section class="page-section bg-dark-1 light-content " id="about">
                 
 
                         <div class="container mt-5" >
-                           <form action="POST">
+                           <form method="POST" enctype="multipart/form-data">
                             
-                            <input type="file" name="fileToUpload" id="fileToUpload">
-                            <input type="text" placeholder="Blog Title" class="form-control bg-dark text-white my-3 text-center">
-                            <textarea class="form-control bg-dark text-white my-3 "></textarea>
-                            <button class="btn btn-dark">Add Post</button>
+                            <input type="file" class="form-control" name="image" >
+                            <input type="text" name="title" placeholder="Blog Title" class="form-control bg-dark text-white my-3 text-center">
+                            <textarea name="sub" class="form-control bg-dark text-white my-3 "></textarea>
+                            <button name="new_post" class="btn btn-dark">Add Post</button>
                            </form>
                         </div>
 
