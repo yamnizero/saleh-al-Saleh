a3ce55c42ff6f26a566ccafd4490cf6b7c1f6b64
mohammed jamal <yamni.zero@gmail.com>
2024-04-01 02:15:57 +0200

    -show blog in dashboard

--- admin/code-blog.php
+++ admin/code-blog.php
@@ -1,11 +1,11 @@
 <?php
 
-define('DB_SERVER',"localhost");
-define('DB_USERNAME',"root");
-define('DB_PASSWORD',"");
-define('DB_DATABASE',"saleh_al_saleh");
+// define('DB_SERVER',"localhost");
+// define('DB_USERNAME',"root");
+// define('DB_PASSWORD',"");
+// define('DB_DATABASE',"saleh_al_saleh");
 
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+// $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 if (!$conn) {
    echo "<h3 class=container bg-dark text-center p-3 text-warning rounded-lg mt-5>Not able establish database Connection</h3>";
--- admin/index.php
+++ admin/index.php
@@ -1,31 +1,37 @@
 <?php
-    include('includes/header.php')
+include('includes/header.php');
+include "code-blog.php"
 ?>
 
+
+<div class="row  row-cols-1 row-cols-md-3 g-4">
     
-    <div class="row">
-        <div class="col-md-3 mb-4">
-            <div class="card card-body p-3">
-                <p class=" text-sm text-capitalize fw-bold">Todday Mo</p>
-                <h5 class="fw-bold mb-0"> 2 </h5>
 
+    <?php foreach ($query as $q) { ?>
+    
+        <div class="col">
+        <div class="card">
+            <img src="<?= "uploads/" . $q['image'] ?>" class="card-img-top" alt="Image Description" />
+            <div class="card-body">
+                <h5 class="card-title"><?= $q['title'] ?></h5>
+                <p class="card-text">
+                <?= $q['sub'] ?>
+                </p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted"><?= $q['created']?></small>
             </div>
         </div>
-        <div class="col-md-3 mb-4">
-            <div class="card card-body p-3">
-                <p class=" text-sm text-capitalize fw-bold">Todday Mo</p>
-                <h5 class="fw-bold mb-0"> 2 </h5>
-
-            </div>
         </div>
-        <div class="col-md-3 mb-4">
-            <div class="card card-body p-3">
-                <p class=" text-sm text-capitalize fw-bold">Todday Mo</p>
-                <h5 class="fw-bold mb-0"> 2 </h5>
+        
+    
+    <?php } ?>
+    <!-- <div class="col-md-3 mb-4">
+        <div class="card card-body p-3">
+            <p class=" text-sm text-capitalize fw-bold">Todday Mo</p>
+            <h5 class="fw-bold mb-0"> 2 </h5>
 
-            </div>
         </div>
-    </div>
+    </div> -->
 
-
-
+</div>
