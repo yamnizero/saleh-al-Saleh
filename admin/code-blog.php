8acb90ce8d2d1bab948f22324de801c3bb3291b2
mohammed jamal <yamni.zero@gmail.com>
2024-04-03 02:16:13 +0200

    Domin

--- admin/code-blog.php
+++ admin/code-blog.php
@@ -1,9 +1,14 @@
 <?php
 
+// define('DB_SERVER',"localhost");
+// define('DB_USERNAME',"root");
+// define('DB_PASSWORD',"");
+// define('DB_DATABASE',"saleh_al_saleh");
+
 define('DB_SERVER',"localhost");
-define('DB_USERNAME',"root");
-define('DB_PASSWORD',"");
-define('DB_DATABASE',"saleh_al_saleh");
+define('DB_USERNAME',"ruknam5_saleh");
+define('DB_PASSWORD',"Salehalsaleh@12345");
+define('DB_DATABASE',"ruknam5_saleh");
 
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
