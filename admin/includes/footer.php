ba2d3256d2adb007a2d45a65e3f6ca86f49aa5b0
mohammed jamal <yamni.zero@gmail.com>
2024-04-03 02:14:08 +0200

    css

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,11 +7,11 @@
                         document.write(new Date().getFullYear())
                     </script>,
                     made with <i class="fa fa-heart"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
+                    <a href="https://phoenix-code.com" class="font-weight-bold" target="_blank">Phoenix Code</a>
                     for a better web.
                 </div>
             </div>
-            <div class="col-lg-6">
+            <!-- <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
                         <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
@@ -26,7 +26,7 @@
                         <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
                     </li>
                 </ul>
-            </div>
+            </div> -->
         </div>
     </div>
 </footer>
